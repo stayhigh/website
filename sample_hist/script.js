4d4727b07741619eb8541858b12812d68416e7e7
stayhigh <redhero0702@gmail.com>
2014-03-19 00:01:07 +0800

    jQuery animation
    update jQuery animate

---
+++ sample_hist/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+   $('div').mouseenter(function() {
+       $(this).animate({
+           height: '+=20px'
+       });
+   });
+   $('div').mouseleave(function() {
+       $(this).animate({
+           height: '-=20px'
+       }); 
+   });
+   $('div').click(function() {
+       $(this).toggle(1000);
+   }); 
+});
