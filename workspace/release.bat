0d5f0cf31f7d49442f01dbebb155c251375e4980
stayhigh <redhero0702@gmail.com>
2014-03-23 23:47:33 +0800

    upload batch files for testrun and release

---
+++ workspace/release.bat
@@ -0,0 +1,3 @@
+echo update to google app engine
+python "C:\Program Files (x86)\Google\google_appengine\appcfg.py" update helloworld
+pause
---
+++ workspace/testrun.bat
@@ -0,0 +1,2 @@
+cd C:\Users\john_wang\Documents\GitHub\website\workspace
+python "C:\Program Files (x86)\Google\google_appengine\dev_appserver.py" helloworld
